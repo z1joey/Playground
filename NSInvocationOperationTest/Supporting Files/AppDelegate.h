cba65c987de781dd78efd632741540501489b048
Yi ZHANG <z1joey@icloud.com>
2019-01-08 17:56:17 +0800

    创建了NSInvocationOperationTest

---
+++ NSInvocationOperationTest/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSInvocationOperationTest
+//
+//  Created by Yi Zhang on 8/1/19.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
